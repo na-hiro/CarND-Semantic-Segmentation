501ea918a9752fe676ed09a4d38d5c5df2611a3a
na-hiro <hinagai.5963@gmail.com>
2018-02-17 20:01:44 +0900

    Reflection of review results

--- main.py
+++ main.py
@@ -106,11 +106,18 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     correct_label = tf.reshape(correct_label, (-1, num_classes))
     # loss function = cross_entropy_loss
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits= logits, labels= correct_label))
+
+    # addition of regularization loss...(item pointed out in 1st review)
+    reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    reg_coeff = 1 #0.01
+    loss = cross_entropy_loss + reg_coeff * sum(reg_losses)
+
     # optimizer
     optimizer = tf.train.AdamOptimizer(learning_rate= learning_rate)
     train_op = optimizer.minimize(cross_entropy_loss)
 
-    return logits, train_op, cross_entropy_loss
+    # modified loss val(item pointed out in 1st review)
+    return logits, train_op, loss #cross_entropy_loss
 tests.test_optimize(optimize)
 
 
